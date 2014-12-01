43e6e07a7e707279a3ee4803585856b023c27653
Henk Holtrop <henk_holtrop@live.nl>
2014-12-01 13:55:08 +0100

    Last update of the day, added a bit of animation(doenst work yet)

---
+++ js/notification.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('.error-notification').animate({"bottom: +=10px"}).animate({"bottom: -10px"}).delay(150).animate({"bottom: +=10px"}).animate({"bottom: -1000px"});
+});
