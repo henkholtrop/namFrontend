f810debb505a28b25643f1a26d23b082a869ac64
Henk Holtrop <henk_holtrop@live.nl>
2014-12-03 14:16:28 +0100

    last commit of the day WIP

--- js/load-box.js
+++ js/load-box.js
@@ -1,14 +1,45 @@
 $(document).ready(function() {
-  $('.button-add').click(function() {
-    $('.add-shadow').css('display','block');
-  });
-  $('.close-add').click(function() {
-    $('.add-shadow').css('display','none');
-  });
-});
+  
+  var closePopup = '.close-add'
 
-$(document).ready( function() {
-    $(".button-add").click(function() {
-        $(".add-shadow").load("add-case.html");
-    });
+  $('.button-add-case').click(function() {
+    $('.add-case-popup').css('display','block');
+  });
+  $('.button-edit-case').click(function() {
+    $('.edit-case-popup').css('display','block');
+  });
+  $('.button-delete-case').click(function() {
+    $('.delete-case-popup').css('display','block');
+  });
+
+  $(closePopup).click(function() {
+    $('.add-case-popup').css('display','none');
+  });
+  $(closePopup).click(function() {
+    $('.edit-case-popup').css('display','none');
+  });
+  $(closePopup).click(function() {
+    $('.delete-case-popup').css('display','none');
+  });
+
+  //Users
+  $('.button-invite-user').click(function() {
+    $('.invite-user-popup').css('display','block');
+  });
+  $('.button-edit-user').click(function() {
+    $('.edit-user-popup').css('display','block');
+  });
+  $('.button-delete-user').click(function() {
+    $('.delete-user-popup').css('display','block');
+  });
+
+  $(closePopup).click(function() {
+    $('.invite-user-popup').css('display','none');
+  });
+  $(closePopup).click(function() {
+    $('.edit-user-popup').css('display','none');
+  });
+  $(closePopup).click(function() {
+    $('.delete-user-popup').css('display','none');
+  });
 });
